a228dcbe2fe54198e93a5d308f3cf72a93fe6b5e
Szkola <Szkola@szkola.com>
2021-10-29 13:20:51 +0200

    Added couple new features to IOrganizationStore.cs

--- ITManagerProject/Stores/Interfaces/IOrganizationStore.cs
+++ ITManagerProject/Stores/Interfaces/IOrganizationStore.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -7,8 +8,17 @@ namespace ITManagerProject.Stores.Interfaces
 {
     public interface IOrganizationStore<TOrganization, TUser> : IDisposable where TOrganization : IDisposable where TUser : class
     {
+        Task CreateOrganizationAsync(string organizationName, CancellationToken cancellationToken);
+        Task<bool> ExistsByNameAsync(string organizationName, CancellationToken cancellationToken);
+        Task<bool> ExistsByIdAsync(string organizationId, CancellationToken cancellationToken);
+        Task<bool> ExistsByUserAsync(TUser user, CancellationToken cancellationToken);
+        Task<bool> ChangeOrganizationNameAsync(string oldOrganizationName, CancellationToken cancellationToken);
+        Task AddToOrganizationAsync(string organizationName, TUser user, CancellationToken cancellationToken);
+        Task RemoveFromOrganizationAsync(string organizationName, TUser user, CancellationToken cancellationToken);
+        Task AddToOrganizationRangeAsync(string organizationName, List<TUser> users, CancellationToken cancellationToken);
+        Task RemoveFromOrganizationRangeAsync(string organizationName, List<TUser> users,
+            CancellationToken cancellationToken);
         Task<List<TUser>> GetUsersFromOrganizationByIdAsync(string organizationId, CancellationToken cancellationToken);
-        
         Task<List<TUser>> GetUsersFromOrganizationByNameAsync(string organizationName, CancellationToken cancellationToken);
     }
 }
