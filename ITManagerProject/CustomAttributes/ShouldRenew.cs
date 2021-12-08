f3516cf284ea52a4346cc66816701e075f60184a
ISmellYu <lukaszszostak10@gmail.com>
2021-12-08 02:08:09 +0100

    Update couple things

--- ITManagerProject/CustomAttributes/ShouldRenew.cs
+++ ITManagerProject/CustomAttributes/ShouldRenew.cs
@@ -1,4 +1,56 @@
-﻿namespace ITManagerProject.CustomAttributes
+﻿using ITManagerProject.Managers;
+using ITManagerProject.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace ITManagerProject.CustomAttributes
 {
-    
+    public class ShouldRenew : TypeFilterAttribute
+    {
+        public ShouldRenew() : base(typeof(ShouldRenewBase))
+        {
+            Arguments = new object[] { true };
+        }
+
+        public ShouldRenew(bool shouldBeIn) : base(typeof(ShouldRenewBase))
+        {
+            Arguments = new object[] {shouldBeIn};
+        }
+    }
+
+    internal class ShouldRenewBase : ActionFilterAttribute
+    {
+        private readonly OrganizationManager<Organization> _organizationManager;
+        private readonly UserManager<User> _userManager;
+        private readonly bool _shouldBeIn;
+        
+        
+        public ShouldRenewBase(OrganizationManager<Organization> organizationManager, UserManager<User> userManager, bool shouldBeIn)
+        {
+            _organizationManager = organizationManager;
+            _userManager = userManager;
+            _shouldBeIn = shouldBeIn;
+        }
+
+        public override async void OnActionExecuting(ActionExecutingContext context)
+        {
+            var user = await _userManager.GetUserAsync(context.HttpContext.User);
+            var status = await _organizationManager.CheckIfInAnyOrganizationAsync(user);
+            if (_shouldBeIn)
+            {
+                if (!status)
+                {
+                    context.Result = new RedirectToActionResult("Index", "Home", null);
+                }
+            }
+            else
+            {
+                if (status)
+                {
+                    context.Result = new RedirectToActionResult("Index", "Home", null);
+                }
+            }
+        }
+    }
 }
--- ITManagerProject/Managers/NotificationManager.cs
+++ ITManagerProject/Managers/NotificationManager.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using ITManagerProject.Contexts;
@@ -25,12 +26,100 @@ namespace ITManagerProject.Managers
             _organizationManager = organizationManager;
         }
 
-        public async Task<bool> AddNotification()
+        public async Task<bool> AddNotification(string title, string body, int organizationId)
         {
             ThrowIfDisposed();
-            return false;
+
+            
+            return true;
         }
         
+        public async Task<bool> AddNotification(string title, string body, int organizationId, int userId)
+        {
+            ThrowIfDisposed();
+
+            var notification = new Notification
+            {
+                Title = title,
+                Body = body,
+            };
+
+            _context.Notifications.Add(notification);
+            await _context.SaveChangesAsync();
+
+            var organizationNotification = new OrganizationNotification
+            {
+                OrganizationId = organizationId,
+                NotificationId = notification.Id,
+                AuthorId = userId
+                
+            };
+
+            _context.OrganizationNotifications.Add(organizationNotification);
+            await _context.SaveChangesAsync();
+
+            return true;
+        }
+        
+        public async Task<bool> RemoveNotification(int notificationId)
+        {
+            ThrowIfDisposed();
+
+            var notification = await Notifications.FirstOrDefaultAsync(n => n.Id == notificationId);
+
+            if (notification == null)
+            {
+                return false;
+            }
+
+            _context.Notifications.Remove(notification);
+            await _context.SaveChangesAsync();
+
+            return true;
+        }
+        
+        public async Task<bool> CheckIfExists(int notificationId)
+        {
+            ThrowIfDisposed();
+
+            return await Notifications.AnyAsync(n => n.Id == notificationId);
+        }
+        
+        public async Task<List<Notification>> GetNotifications(int organizationId)
+        {
+            ThrowIfDisposed();
+
+            var organizationNotifications = await OrganizationNotifications.Where(on => on.OrganizationId == organizationId).ToListAsync();
+
+            var notifications = new List<Notification>();
+
+            foreach (var organizationNotification in organizationNotifications)
+            {
+                var notification = await Notifications.FirstOrDefaultAsync(n => n.Id == organizationNotification.NotificationId);
+                notifications.Add(notification);
+            }
+
+            return notifications;
+        }
+        
+        public async Task<List<Notification>> GetNotifications(int organizationId, int userId)
+        {
+            ThrowIfDisposed();
+
+            var organizationNotifications = await OrganizationNotifications.Where(on => on.OrganizationId == organizationId && on.AuthorId == userId).ToListAsync();
+
+            var notifications = new List<Notification>();
+
+            foreach (var organizationNotification in organizationNotifications)
+            {
+                var notification = await Notifications.FirstOrDefaultAsync(n => n.Id == organizationNotification.NotificationId);
+                notifications.Add(notification);
+            }
+
+            return notifications;
+        }
+        
+
         public void Dispose()
         {
             Dispose(true);
